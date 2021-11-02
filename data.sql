44732866fb3a601234cb48bc6612672cdea07ac7
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-02 17:16:28 -0500

    Insert last info in animals table

--- data.sql
+++ data.sql
@@ -3,4 +3,4 @@
 INSERT INTO animals VALUES (1, 'Agumon', '2020-02-03', 0 , true, 10.23);
 INSERT INTO animals VALUES (2, 'Gabumon', '2018-11-15', 2 , true, 8);
 INSERT INTO animals VALUES (3, 'Pikachu', '2021-06-07', 1 , false, 15.04);
-
+INSERT INTO animals VALUES (4, 'Devimon', '2017-05-12', 5 , true, 11);
