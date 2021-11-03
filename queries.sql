63551ac2c1396f9ce8cc33e0d142abe725c9bb14
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-02 20:04:55 -0500

    Add specification to data.sql file and queries.sql file

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name LIKE '%mon';
-SELECT * from animals WHERE date_of_birth BETWEEN 2016 AND 2019;
-SELECT * from animals WHERE neutered IS 'true' AND escape_attempts < 3;
-SELECT date_of_birth from animals WHERE name IS 'Agumon' OR 'Pikachu';
-SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
-SELECT * from animals WHERE neutered IS 'true';
-SELECT * from animals WHERE NOT name = 'Gabumon';
-SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = 'true';
+SELECT * FROM animals WHERE NOT name = 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
