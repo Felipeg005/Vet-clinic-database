c3425d62239e09f3db4983b6aa5a592302ff4c85
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-04 17:15:24 -0500

    Fix schema.sql file Integer code

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id int NOT NULL IDENTITY(1,1),
+    id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
     PRIMARY KEY (id),
     name char(250),
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
     weight_kg decimal,
-    species string
+    species char(250)
 );
