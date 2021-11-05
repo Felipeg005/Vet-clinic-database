effdb68a79d275733d5986e4b05e982ea64f23a4
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-04 23:05:58 -0500

    Insert required data

--- data.sql
+++ data.sql
@@ -9,3 +9,17 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', '2005-06-12', 1 , true, 45);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', '2005-06-07', 7 , true, 20.4);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3 , true, 17);
+
+INSERT INTO species (name) VALUES ('Digimon');
+INSERT INTO species (name) VALUES ('Pokemon');
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+BEGIN;
+UPDATE animals SET species_id = (SELECT name FROM species WHERE name LIKE '%mon');
+COMMIT;
