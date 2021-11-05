4dc8b25b2fb70a06d009b53e9bfdfb4b797c0716
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-04 22:46:02 -0500

    Creates foreign keys and new tables

--- schema.sql
+++ schema.sql
@@ -1,4 +1,20 @@
 /* Database schema to keep the structure of entire database. */
+DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS species;
+
+CREATE TABLE species (
+    id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
+    PRIMARY KEY (id),
+    name char(250)
+);
+
+CREATE TABLE owners (
+    id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
+    PRIMARY KEY (id),
+    full_name char(250),
+    age int
+);
 
 CREATE TABLE animals (
     id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
@@ -8,5 +24,8 @@ CREATE TABLE animals (
     escape_attempts integer,
     neutered boolean,
     weight_kg decimal,
-    species char(250)
+    species_id int,
+    owner_id int,
+    FOREIGN KEY(species_id) REFERENCES species(id) ON DELETE CASCADE,
+    FOREIGN KEY(owner_id) REFERENCES owners(id) ON DELETE CASCADE
 );
